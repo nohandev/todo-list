96373c88325042612a7c9ece450d08cc6d12b2f3
nohandev <nohanpablo759@gmail.com>
2025-10-22 21:35:34 -0300

    adding logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Logo from './assets/Logo.png'
 const App = () => {
   return (
-    <div>
-     Hello World 
-    </div>
+    <section className='h-dvh bg-gray-500'>
+      <div className='h-48 bg-gray-700 flex justify-center items-center'>
+        <img src={Logo} alt='Logo Rocket'/>
+      </div>
+    </section>
   )
 }
 
